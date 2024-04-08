79e128655bfb0ddcea9fc7e1e4b2ae8de8ecd615
Christopher V <ChrisRVill@gmail.com>
2024-04-07 23:26:44 -0700

    Completely removed HTML in an attempt to solve nonfunctioning HTMX. Server GET/POST funcs are set up. DoMath func set up

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"io"
+	"strconv"
 	"text/template"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type Templates struct {
@@ -18,20 +20,76 @@ func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Co
 
 func newTemplate() *Templates {
 	return &Templates{
-		templates: template.Must(template.ParseGlob("site/*html")),
+		templates: template.Must(template.ParseGlob("site/*.html")),
 	}
 }
 
-type Calculator struct {
-	CurrentValue input
-	SecondValue  input
-	Operator     string
+type Value struct {
+	Name string
 }
 
-type input struct {
-	number float64
+type Page struct {
+	Value Value
+}
+
+func newValue() Value {
+	return Value{
+		Name: "0",
+	}
+}
+
+func newPage() Page {
+	return Page{
+		Value: newValue(),
+	}
+}
+
+func doMath(firstNum, secondNum, operator string) string {
+	answer := ""
+	switch operator {
+	case "+":
+		f, _ := strconv.ParseFloat(firstNum, 64)
+		s, _ := strconv.ParseFloat(secondNum, 64)
+		answerFloat := f + s
+		answer = strconv.FormatFloat(answerFloat, 'f', -1, 64)
+	}
+
+	return answer
 }
 
 func main() {
-	fmt.Printf("hello world")
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	page := newPage()
+	e.Renderer = newTemplate()
+
+	storedValue := ""
+	operator := ""
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", page)
+	})
+
+	e.POST("/update", func(c echo.Context) error {
+		value := c.FormValue("numpad")
+		fmt.Println(value)
+		newValue := page.Value.Name + value
+		page.Value.Name = newValue
+		return c.Render(200, "display", page)
+	})
+
+	e.POST("/next", func(c echo.Context) error {
+		storedValue = page.Value.Name
+		page.Value.Name = "0"
+		operator = c.FormValue("op")
+		return c.Render(200, "display", page)
+	})
+
+	e.GET("/return", func(c echo.Context) error {
+		page.Value.Name = doMath(storedValue, page.Value.Name, operator)
+		return c.Render(200, "display", page)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
