cbb892dc7ddd413d200f0c57586d3890dd4db891
Christopher V <ChrisRVill@gmail.com>
2024-05-04 20:00:10 -0700

    Changed from a web calc to a cli calc

--- main.go
+++ main.go
@@ -1,95 +1,47 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
-	"strconv"
-	"text/template"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"os"
 )
 
-type Templates struct {
-	templates *template.Template
-}
-
-func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
-func newTemplate() *Templates {
-	return &Templates{
-		templates: template.Must(template.ParseGlob("site/*.html")),
-	}
-}
-
-type Value struct {
-	Name string
-}
-
-type Page struct {
-	Value Value
-}
-
-func newValue() Value {
-	return Value{
-		Name: "0",
-	}
-}
-
-func newPage() Page {
-	return Page{
-		Value: newValue(),
-	}
-}
-
-func doMath(firstNum, secondNum, operator string) string {
-	answer := ""
-	switch operator {
-	case "+":
-		f, _ := strconv.ParseFloat(firstNum, 64)
-		s, _ := strconv.ParseFloat(secondNum, 64)
-		answerFloat := f + s
-		answer = strconv.FormatFloat(answerFloat, 'f', -1, 64)
-	}
-
-	return answer
-}
-
 func main() {
-	e := echo.New()
-	e.Use(middleware.Logger())
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	welcomeMessage()
+	var eq []string
+	for {
+		scanner.Scan()
+		//fmt.Printf("Current first input: %f, Current second input: %f \n", numInput[len((numInput))-2], numInput[len(numInput)-1])
 
-	page := newPage()
-	e.Renderer = newTemplate()
+		output := scanner.Text()
+		eq = append(eq, output)
+		// num, err := strconv.ParseFloat(output, 64)
+		// if err != nil {
+		if len(output) > 1 {
+			if output == "quit" {
+				return
+			}
+			// if output == "clear" {
+			// 	numInput = [][]float64{0, 0}
+			// }
+		}
 
-	storedValue := ""
-	operator := ""
+		// }
+		// numInput = append(numInput, num)
 
-	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", page)
-	})
+		fmt.Println(eq[:])
+	}
+}
 
-	e.POST("/update", func(c echo.Context) error {
-		value := c.FormValue("numpad")
-		fmt.Println(value)
-		newValue := page.Value.Name + value
-		page.Value.Name = newValue
-		return c.Render(200, "display", page)
-	})
+func welcomeMessage() {
+	fmt.Println("Welcome!")
+	help()
+	fmt.Println("What do you want calculated?")
+}
 
-	e.POST("/next", func(c echo.Context) error {
-		storedValue = page.Value.Name
-		page.Value.Name = "0"
-		operator = c.FormValue("op")
-		return c.Render(200, "display", page)
-	})
-
-	e.GET("/return", func(c echo.Context) error {
-		page.Value.Name = doMath(storedValue, page.Value.Name, operator)
-		return c.Render(200, "display", page)
-	})
-
-	e.Logger.Fatal(e.Start(":8080"))
+func help() {
+	fmt.Println("supported operators: '+', '-', '*', or '/'")
+	fmt.Println("Alt commands: 'clear', 'history', or 'quit'")
 }
